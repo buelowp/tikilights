1b0891d105e4293ba8c3da8f6cbca3ad4f624958
Peter Buelow <peter.buelow@lifefitness.com>
2019-06-12 10:40:57 -0500

    Testing well
    * Got sleep times to work
    * Fixed it to use a direct weather request
    * Added some logging for debug
    * Refactored some calculations
    * Moved sleep to the top to avoid doing work we shouldn't

--- src/tikilights.ino
+++ src/tikilights.ino
@@ -2,10 +2,12 @@
 #include <SunSet.h>
 #define ARDUINOJSON_ENABLE_PROGMEM 0
 #include <ArduinoJson.h>
+#include <HttpClient.h>
 #include "TikiCandle.h"
 #include "Torch.h"
 
-#define APP_VERSION			80
+#define APP_VERSION			92
+#define API_KEY             "65e4c00704a8f00c6a1484b4f9eba63c"
 
 PRODUCT_ID(985);
 PRODUCT_VERSION(APP_VERSION);
@@ -15,24 +17,23 @@ const uint8_t _usDSTStart[22] = { 10, 8,14,13,12,10, 9, 8,14,12,11,10, 9,14,13,1
 const uint8_t _usDSTEnd[22]   = { 3, 1, 7, 6, 5, 3, 2, 1, 7, 5, 4, 3, 2, 7, 6, 5, 4, 2};
 SunSet sun;
 int tikiCurrentProgram;
-bool runProgramSet;
-bool programSetDone;
-bool gReverseDirection;
-bool setupCandlesDone;
 int g_appId;
 int g_timeZone;
-int g_oscillations;
-int g_sleepTime;
 double g_sunset;
-double g_testSunset;
-int g_convertSunset;
 double g_minsPastMidnight;
-double g_hour;
-time_t g_lastSync;
 bool g_validRunTime;
 int g_jsonError;
+int g_temp;
+int g_httpResponse;
 TikiCandle candle;
-TCPClient g_client;
+
+HttpClient http;
+http_header_t headers[] = {
+    { "Accept" , "*/*"},
+    { NULL, NULL } // NOTE: Always terminate headers will NULL
+};
+http_request_t request;
+http_response_t response;
 
 STARTUP(WiFi.selectAntenna(ANT_INTERNAL));
 
@@ -65,38 +66,38 @@ void syncTime()
     Time.zone(currentTimeZone());
     sun.setCurrentDate(Time.year(), Time.month(), Time.day());
     sun.setTZOffset(currentTimeZone());
-    g_lastSync = Particle.timeSyncedLast();
 }
 
 int remainingSleepTime()
 {
-    int mpm = Time.hour() * 60 + Time.minute();
-    int remain = 3600;
-    int interval = (g_sunset - mpm) * 60;
-    g_convertSunset = (int)g_sunset;
-    g_sleepTime = 3600;
-    g_testSunset = g_sunset;
-    g_hour = Time.hour();
+    int remain = 60 * 60;
     g_minsPastMidnight = Time.hour() * 60 + Time.minute();
-
+    int interval = (g_sunset - g_minsPastMidnight) * 60;
+ 
     /* Shouldn't get here */
-    if (g_sunset < mpm)
+    if (g_sunset < (g_minsPastMidnight - 60))
         return remain;
 
-    if (interval < remain)
+    if (interval > remain)
         return remain;
 
     remain = interval;
-    g_sleepTime = remain;
 
     return remain; 
 }
 
 void goToSleep()
 {
+    int rst = remainingSleepTime();
     FastLED.clear();
     FastLED.show();
-//    System.sleep(SLEEP_MODE_DEEP, remainingSleepTime());
+
+    String time = String("Going to sleep for ") + String(rst) + String(" seconds, sunset at ") + String(g_sunset);
+    Serial.println(time);
+    Particle.publish("SleepTime", time, PRIVATE);
+    delay(1000);
+    Particle.process();
+    System.sleep(SLEEP_MODE_DEEP, remainingSleepTime());
 }
 
 bool validRunTime()
@@ -104,87 +105,66 @@ bool validRunTime()
     g_sunset = sun.calcSunset();
     g_minsPastMidnight = Time.hour() * 60 + Time.minute();
     g_validRunTime = true;
-    g_hour = Time.hour();
 
-    if (g_minsPastMidnight >= g_sunset)
-        return true;
+    if (g_minsPastMidnight >= g_sunset - 60)
+        return g_validRunTime;
     
     g_validRunTime = false;
-    return false;
+    return g_validRunTime;
 }
 
 void setProgram()
 {
-    char server[] = "api.openweathermap.org";
-    char data[1024];
-    int port = 80;
-    int value = 75;
-    const size_t capacity = JSON_ARRAY_SIZE(1) + 
-                            JSON_OBJECT_SIZE(1) + 
-                            2*JSON_OBJECT_SIZE(2) + 
-                            JSON_OBJECT_SIZE(4) + 
-                            JSON_OBJECT_SIZE(5) + 
-                            JSON_OBJECT_SIZE(6) + 
-                            JSON_OBJECT_SIZE(13) + 366;
+    String path = String("/data/2.5/weather?units=imperial&zip=60005&appid=");
 
-    if (g_client.connect(server, port)) {
-        Serial.println("connected");
-        g_client.println("GET /data/2.5/weather?id=65e4c00704a8f00c6a1484b4f9eba63c&units=imperial&zip=60005 HTTP/1.1");
-        g_client.println("Host: api.openweathermap.org");
-        g_client.println("Content-Length: 0");
-        g_client.println();
+    path.concat(API_KEY);
+    request.hostname = "api.openweathermap.org";
+    request.port = 80;
+    request.path = path.c_str();
 
-        DynamicJsonDocument doc(capacity);
-        DeserializationError error = deserializeJson(doc, g_client);
+    http.get(request, response, headers);
+    if (response.status == 200) {
+        DynamicJsonDocument doc(1024);
+        auto error = deserializeJson(doc, response.body.c_str());
         if (error) {
-            Serial.print(F("deserializeJson() failed: "));
+            Serial.print(F("deserializeJson() failed with code "));
             Serial.println(error.c_str());
             g_jsonError = static_cast<int>(error.code());
-            return;
         }
-        g_client.stop();
-        value = doc["main"]["temp"].as<int>();
+        g_temp = doc["main"]["temp"];
+        Serial.print("Got temperature ");
+        Serial.println(g_temp);
     }
     else {
         Serial.println("connection failed");
+        Serial.println(path);
+        Serial.print("Response code ");
+        Serial.println(response.status);
+        g_httpResponse = response.status;
     }
 
-	switch (value) {
-	case 0:
-		candle.init(HUE_PURPLE, HUE_PURPLE - 10, HUE_PURPLE + 10, 25, 10);
-		break;
-	case 1:
-		candle.init(HUE_BLUE, HUE_BLUE - 10, HUE_BLUE + 10, 25, 10);
-		break;
-	case 2:
-		candle.init(HUE_GREEN, HUE_GREEN - 10, HUE_GREEN + 10, 25, 10);
-		break;
-	case 3:
-		candle.init(HUE_YELLOW, HUE_YELLOW - 10, HUE_YELLOW + 10, 25, 10);
-		break;
-	case 4:
-		candle.init(HUE_RED, 0, HUE_RED + 20, 25, 10);
-		break;
-	}
+    if (g_temp >= 70)
+        candle.init(HUE_RED, 0, HUE_RED + 20, 25, 10);
+    else if (g_temp >= 65)
+        candle.init(HUE_YELLOW, HUE_YELLOW - 10, HUE_YELLOW + 10, 25, 10);
+    else if (g_temp >= 60)
+        candle.init(HUE_GREEN, HUE_GREEN - 10, HUE_GREEN + 10, 25, 10);
+    else if (g_temp >= 55)
+        candle.init(HUE_BLUE, HUE_BLUE - 10, HUE_BLUE + 10, 25, 10);
+    else
+        candle.init(HUE_PURPLE, HUE_PURPLE - 10, HUE_PURPLE + 10, 25, 10);
 }
 
 void setup()
 {
     g_appId = APP_VERSION;
     g_timeZone = CST_OFFSET;
-    g_oscillations = 0;
-    g_sleepTime = 0;
-    g_testSunset = 0.0;
-    g_convertSunset = 0;
-    g_hour = 0;
-    g_lastSync = 0;
     g_jsonError = false;
+    g_temp = 0;
 
     Particle.variable("appid", g_appId);
     Particle.variable("sunset", g_sunset);
-    Particle.variable("sleep", g_sleepTime); 
-    Particle.variable("testsun", g_testSunset);
-    Particle.variable("castsun", g_convertSunset);
+    Particle.variable("temp", g_temp);
     Particle.variable("mpm", g_minsPastMidnight);
     Particle.variable("jsonerr", g_jsonError);
 
@@ -200,9 +180,14 @@ void setup()
 
 void loop()
 {
+    static bool programSetDone = false;
+    if (!validRunTime()) {
+        goToSleep();
+    }
+
 	if ((Time.minute() == 1) && !programSetDone) {
 		Serial.printf("Turning wifi on\n");
-//		WiFi.on();
+		WiFi.on();
         waitUntil(WiFi.ready);
         syncTime();
         setProgram();
@@ -210,13 +195,9 @@ void loop()
 	}
 
 	if (Time.minute() != 1) {
-//		WiFi.off();
+		WiFi.off();
 		programSetDone = false;
 	}
 
-    if (!validRunTime()) {
-        goToSleep();
-    }
-    else
-	    candle.run();
+    candle.run();
 }
